2867b95088f4ee1a9141078a7b779bd8768a4eb2
sanaebadi <sana@journeyhero.net>
2023-01-18 20:07:34 +0200

    foor with more than 1 var

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int numberOne;
+        int numberTwo;
+        for (numberOne = 2, numberTwo = 1; numberTwo * numberOne <= 100; numberTwo++) {
+            System.out.println("The result of " + "(" + numberOne + " * " + numberTwo +  ")"+ " is = " +  (numberOne * numberTwo));
+        }
+
+
+    }
+}
